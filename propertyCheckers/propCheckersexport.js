7c15cf3c7677f7e60bfceb102a47d7490a79e26c
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2025-10-21 11:57:12 +0100

    Refactor imports and variable assignments in propCheckersexport.js

--- propertyCheckers/propCheckersexport.js
+++ propertyCheckers/propCheckersexport.js
@@ -1,10 +1,9 @@
-const characterFrequencyMap = require('./propertyCheckers/character_frequency_map');
-const palindrome_check = require('./propertyCheckers/palindrome_check')
-const unique_characters = require('./propertyCheckers/unique_characters_check')
-const word_count = require('./propertyCheckers/word_count')
-const sha256_hash = require('./propertyCheckers/createSHA')
-const character_frequency_map = require('./propertyCheckers/character_frequency_map')
-const length = require('./propertyCheckers/length');
+const palindrome_check = require('./palindrome_check')
+const unique_characters = require('./unique_characters_check')
+const word_count = require('./word_count')
+const sha256_hash = require('./createSHA')
+const character_frequency_map = require('./character_frequency_map')
+const length = require('./length');
 
 function pce(value) {
     const length = length(value);
@@ -12,10 +11,10 @@ function pce(value) {
     const unique_characters = unique_characters(value)
     const word_count = word_count(value)
     const sha256_hash = sha256_hash(value)
-    const character_frequency_map = characterFrequencyMap(value)
+    const character_frequency_map = character_frequency_map(value)
 
     return {
-            length: entry.length,
+            length,
             is_palindrome,
             unique_characters,
             word_count,
